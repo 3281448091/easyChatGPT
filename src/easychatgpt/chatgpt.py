89594d125fc105adb1861103b73f3962fc6c28e4
Rehan Rana <rehanalirana@tuta.io>
2022-12-25 21:51:17 -0500

    adds selenium exception import

--- src/easychatgpt/chatgpt.py
+++ src/easychatgpt/chatgpt.py
@@ -9,6 +9,8 @@ from threading import Thread
 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
+
 
 from easychatgpt.exceptions import NotEnoughInformationException, CouldNotSolveCaptcha
 
